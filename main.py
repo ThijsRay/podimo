1e5430f7c4e30fa2bd4e9e5544f7cc000ca61642
Thijs Raymakers <thijs@raymakers.nl>
2022-04-16 10:05:20 +0200

    Pass credentials in URL

--- main.py
+++ main.py
@@ -28,34 +28,29 @@ tokens = dict()
 token_timeout = 3600 # seconds
 
 def example():
-    return f"Example\n------------\nUsername: example@example.com\nPassword: 123456\nPodcast ID: 12345-abcdef\n\nThe URL will be\nhttps://example%40example.com:123456@{HOST}/feed/12345-abcdef.xml\n\nNote that the username and password should be URL encoded. This can be done with\na tool like https://devpal.co/url-encode/\n" 
+    return f"Example\n------------\nUsername: example@example.com\nPassword: this-is-my-password\nPodcast ID: 12345-abcdef\n\nThe URL will be\nhttps://{HOST}/feed/example%40example.com/this-is-my-password/12345-abcdef.xml\n\nNote that the username and password should be URL encoded. This can be done with\na tool like https://devpal.co/url-encode/\n" 
 
 def authenticate():
     return Response(f"401 Unauthorized.\nYou need to login with the correct credentials for Podimo.\n\n{example()}", 401,
-            {'WWW-Authenticate': 'Basic realm="Login Required"', 'Content-Type': 'text/plain'})
+            {'Content-Type': 'text/plain'})
 
 # Verify if it is actually an email address
 def is_correct_email_address(username):
     return '@' in parseaddr(username)[1]
 
-def token_key(request):
-    auth = request.authorization
-    username = str(auth.username)
-    password = str(auth.password)
+def token_key(username, password):
     key = sha256(b'~'.join([username.encode('utf-8'), password.encode('utf-8')])).hexdigest()
     return key
 
 
 def check_auth(username, password):
     try:
-        username = str(username)
-        password = str(password)
         if len(username) > 256 or len(password) > 256:
             return False
 
         # Check if there is an authentication token already in memory. If so, use that one.
         # If it is expired, request a new token.
-        key = token_key(request)
+        key = token_key(username, password)
         if key in tokens:
             _, timestamp = tokens[key]
             if timestamp < time():
@@ -74,38 +69,33 @@ def check_auth(username, password):
         app.logger.debug(f"An error occurred: {e}")
     return False
 
-def requires_login(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        print(tokens)
-        return f(*args, **kwargs)
-    return decorated
-
 @app.errorhandler(404)
 def not_found(error):
     return Response(f"404 Not found.\n\n{example()}", 401, {'Content-Type': 'text/plain'})
 
 id_pattern = re.compile('[0-9a-fA-F\-]+')
-@app.route('/feed/<podcast_id>.xml')
+@app.route('/feed/<username>/<password>/<podcast_id>.xml')
 @limiter.limit("3/minute")
-@requires_login
-def serve_feed(podcast_id):
+def serve_feed(username, password, podcast_id):
+    # Authenticate
+    username = str(username)
+    password = str(password)
+    if not check_auth(username, password):
+        return authenticate()
     # Check if it is a valid podcast id string
     podcast_id = str(podcast_id)
     if id_pattern.fullmatch(podcast_id) is None:
         return Response("Invalid podcast id format", 404, {})
 
     # Get a list of valid podcasts
-    token, _ = tokens[token_key(request)]
+    token, _ = tokens[token_key(username, password)]
     try:
-        podcasts = podcastsToRss(getPodcasts(token, podcast_id))
+        podcasts = podcastsToRss(username, password, getPodcasts(token, podcast_id))
     except Exception as e:
         exception = str(e)
         if "Podcast not found" in exception:
             return Response("Podcast not found. Are you sure you have the correct ID?", 404, {})
+        print(f"Error while fetching podcasts: {exception}")
         return Response("Something went wrong while fetching the podcasts", 500, {})
     return Response(podcasts, mimetype='text/xml')
 
@@ -262,11 +252,11 @@ def getPodcasts(token, podcast_id):
     result = client.execute(query, variable_values=variables)
     return result
 
-def contentLengthOfUrl(url):
-    token, _ = tokens[token_key(request)]
+def contentLengthOfUrl(username, password, url):
+    token, _ = tokens[token_key(username, password)]
     return head(url, headers=generateHeaders(token)).headers['content-length']
 
-def podcastsToRss(data):
+def podcastsToRss(username, password, data):
     fg = FeedGenerator()
     fg.load_extension('podcast')
 
@@ -283,7 +273,7 @@ def podcastsToRss(data):
         fe.title(episode['title'])
         url = episode['streamMedia']['url']
         mt, enc = guess_type(url)
-        fe.enclosure(url, contentLengthOfUrl(url), mt)
+        fe.enclosure(url, contentLengthOfUrl(username, password, url), mt)
         fe.podcast.itunes_duration(episode['streamMedia']['duration'])
         fe.description(episode['description'])
         fe.pubDate(episode['datetime'])
