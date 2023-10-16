f0d92d3e1dfecb97f4b614679d717aa0d7bc2cfd
Thijs Raymakers <thijs@raymakers.nl>
2023-10-16 18:56:51 +0200

    Accept cookies on Podimo home page

--- main.ts
+++ main.ts
@@ -1,26 +1,53 @@
 import puppeteer from 'npm:puppeteer-extra'
 import StealthPlugin from 'npm:puppeteer-extra-plugin-stealth'
-import { executablePath } from "npm:puppeteer";
+import repl from 'npm:puppeteer-extra-plugin-repl'
+import { Page, Browser, BrowserContext, executablePath } from "npm:puppeteer";
 
-export function puppet() {
-  // puppeteer usage as normal
-  puppeteer
+async function newBrowser() : Promise<Browser> {
+  return puppeteer
+    .use(repl())
     .use(StealthPlugin())
     .launch({
-      headless: "new",
+      // headless: "new",
+      headless: false,
       executablePath: executablePath()
-    }).then(async browser => {
-    console.log('Running tests..')
-    const page = await browser.newPage()
-    await page.goto('https://bot.sannysoft.com')
-    await page.waitForTimeout(5000)
-    await page.screenshot({ path: 'testresult.png', fullPage: true })
-    await browser.close()
-    console.log(`All done, check the screenshot. ✨`)
-  })
+    });
+}
+
+async function newSession(browser: Browser) : Promise<BrowserContext> {
+  return browser.createIncognitoBrowserContext();
+}
+
+async function newPage(session: BrowserContext) : Promise<Page> {
+  return session.newPage();
+}
+
+class PodimoClient {}
+
+async function acceptCookies(page: Page) : Promise<Page> {
+  await page.goto('https://podimo.com');
+  await page.waitForSelector(".cookie")
+  await page.click(".cookie >* button")
+  return page
+}
+
+async function endSession(session: BrowserContext) {
+  await session.close();
+}
+
+async function main() {
+  const browser = await newBrowser();
+
+  const session = await newSession(browser);
+  newPage(session).then(async page => {
+    await acceptCookies(page);
+  });
+  await endSession(session);
+
+  await browser.close();
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  puppet()
+  await main();
 }
