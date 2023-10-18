78b21b0737bd1aa6d8acbb25f2d0e77b80ca7f35
Thijs Raymakers <thijs@raymakers.nl>
2023-10-18 17:04:36 +0200

    Hijack a GraphQL request and request information about podcasts instead

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ import StealthPlugin from 'npm:puppeteer-extra-plugin-stealth'
 import repl from 'npm:puppeteer-extra-plugin-repl'
 import resourceBlock from 'npm:puppeteer-extra-plugin-block-resources';
 import { Page, Browser, BrowserContext, executablePath } from "npm:puppeteer";
-import { RequestInterceptionManager } from 'https://raw.githubusercontent.com/ThijsRay/puppeteer-intercept-and-modify-requests/60706c9e4d81c490ca7d500f2e57a1c2c246f8a9/src/main.ts';
 
 async function newBrowser() : Promise<Browser> {
   return puppeteer
@@ -13,8 +12,8 @@ async function newBrowser() : Promise<Browser> {
     }))
     .use(StealthPlugin())
     .launch({
-      // headless: "new",
-      headless: false,
+      headless: "new",
+      // headless: false,
       executablePath: executablePath(),
     });
 }
@@ -24,14 +23,130 @@ async function newSession(browser: Browser) : Promise<BrowserContext> {
 }
 
 async function newPage(session: BrowserContext) : Promise<Page> {
-  return session.newPage();
+ return session.newPage();
 }
 
-class PodimoClient {}
+const ANIMATION_TIMEOUT = 2500;
 
-const ANIMATION_TIMEOUT = 1000;
+const query = `query PodcastEpisodesResultsQuery($podcastId: String!, $limit: Int!, $offset: Int!, $sorting: PodcastEpisodeSorting) {
+  episodes: podcastEpisodes(
+    podcastId: $podcastId
+    converted: true
+    published: true
+    limit: $limit
+    offset: $offset
+    sorting: $sorting
+  ) {
+    ...EpisodeBase
+  }
+  podcast: podcastById(podcastId: $podcastId) {
+    title
+    description
+    webAddress
+    authorName
+    language
+    images {
+        coverImageUrl
+    }
+  }
+}
 
-async function login(page: Page, username: string, password: string) : Promise<PodimoClient> {
+fragment EpisodeBase on PodcastEpisode {
+  id
+  artist
+  podcastName
+  imageUrl
+  description
+  datetime
+  title
+  audio {
+    url
+    duration
+  }
+  streamMedia {
+    duration
+    url
+  }
+}
+`
+// The request that will be hijacked, and the new payload
+const baseUrl = "https://open.podimo.com/graphql?queryName="
+// const baseUrl = "http://localhost:8080/?queryName=";
+const interceptUrl = baseUrl + "ProfileResultsQuery";
+const operationName = "PodcastEpisodesResultsQuery";
+const targetUrl = baseUrl + operationName;
+
+async function interceptAndModify(page: Page, podcastId: string) {
+  // Setup post data
+const episodesLimit = 50;
+  const payload = JSON.stringify({
+    "operationName": operationName,
+    "query": query,
+    "variables": {
+      "limit": episodesLimit,
+      "offset": 0,
+      "podcastId": podcastId,
+      "sorting": "PUBLISHED_DESCENDING",
+    }
+  });
+
+  // Capture the request, and send a new request
+  const client = await page.target().createCDPSession();
+  client.once('Fetch.requestPaused', (event) => {
+    const { requestId, request } = event;
+
+    client.send('Fetch.fulfillRequest', {
+      requestId,
+      responseCode: 200,
+      body: btoa("{}")
+    }).then(() => {
+        fetch(targetUrl, {
+          method: request.method,
+          headers: request.headers,
+          body: payload
+        }).then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            // Error
+            // {
+            //   errors: [
+            //     {
+            //       message: "User does not have enough privilege",
+            //       locations: [ { line: 36, column: 3 } ],
+            //       path: [ "episodes", 0, "streamMedia" ],
+            //       code: "forbidden",
+            //       extensions: {
+            //         exception: {
+            //           code: "forbidden",
+            //           message: "User does not have enough privilege"
+            //         }
+            //       }
+            //     }
+            //   ],
+            //   data: null
+            // }
+
+            console.log("Finished!");
+          })
+    })
+  });
+
+  // Issue requestPaused events when these requests happen
+  await client.send('Fetch.enable', {
+    patterns: [
+      {
+        urlPattern: interceptUrl,
+        requestStage: "Request"
+      }
+    ]
+  });
+}
+
+async function getPodcastInfo(page: Page, username: string, password: string, podcastId: string) {
+  // Setup the interceptor
+  interceptAndModify(page, podcastId);
+
+  // Login to the web player
   const emailInput = "input[name=email]";
   await page.waitForSelector(emailInput, {"visible": true, "timeout": ANIMATION_TIMEOUT});
   await page.type(emailInput, username);
@@ -43,27 +158,11 @@ async function login(page: Page, username: string, password: string) : Promise<P
   const submitButton = "button[type=submit]";
   await page.waitForSelector(submitButton, {"visible": true, "timeout": ANIMATION_TIMEOUT});
 
-  const client = await page.target().createCDPSession();
-  const interceptManager = new RequestInterceptionManager(client);
-  const interceptConfig : Interception = {
-    urlPattern: "*graphql*",
-    modifyResponse: ({ body }) => {
-      console.log(body);
-      return {
-        body: body
-      }
-    }
-  }
-  await interceptManager.intercept(interceptConfig);
-
+  // Submit and wait
   await Promise.all([
-    page.waitForNavigation({timeout: 5000}),
+    page.waitForNavigation({timeout: ANIMATION_TIMEOUT}),
     page.click(submitButton),
   ]);
-  
-  await page.repl();
-
-  return new PodimoClient();
 }
 
 async function endSession(session: BrowserContext) {
@@ -80,8 +179,9 @@ async function main() {
     await page.setViewport({width: 390, height: 844})
 
     await page.goto('https://open.podimo.com');
+    // await page.goto('http://localhost:8080');
     // page = await acceptCookies(page);
-    const client = await login(page, username, password);
+    await getPodcastInfo(page, username, password, podcastId);
   });
   await endSession(session);
 
